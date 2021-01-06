7c40f721de445dbdcc7db7b05787cabec9e0fd12
Ted Anton <tedjanton@gmail.com>
2021-01-06 13:05:53 -0600

    moving moles

--- whack-a-mole-starter/mole.js
+++ whack-a-mole-starter/mole.js
@@ -9,3 +9,30 @@
 //     }, 1000);
 
 // });
+
+function popUpRandomMole() {
+    let moles = document.getElementsByClassName("wgs__mole-head");
+
+    let moleIdx = Math.floor(Math.random() * 8);
+    console.log(moleIdx);
+
+    moles[moleIdx].classList.remove("wgs__mole-head--hidden");
+
+    setTimeout(() => {
+        hideMole(moles[moleIdx]);
+    }, 1000)
+}
+
+function hideMole(mole) {
+    mole.classList.add("wgs__mole-head--hidden");
+
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 1000)
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 0)
+});
